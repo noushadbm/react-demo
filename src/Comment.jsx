d050aab21de57311d3545375a028d8098f7afbd0
noushadbm <noushadbm@gmail.com>
2023-02-07 19:03:57 +0400

    1 file modified

--- src/Comment.jsx
+++ src/Comment.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 
 class Comment extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      likes: 0,
+    };
+  }
+
+  likeIncrement = () => {
+    console.log('Increment...');
+    var value = this.state.likes;
+    this.setState({
+      likes: ++value,
+    });
+  };
+
+  likeDecrement = () => {
+    var value = this.state.likes;
+    this.setState({
+      likes: --value,
+    });
+  };
+
   render() {
-    return <>
-      <div>This is from comment. Author: {this.props.auther? this.props.auther:"None"}</div>
-    </>;
+    return (
+      <>
+        <div>
+          This is from comment. Author:{' '}
+          {this.props.auther ? this.props.auther : 'None'}
+        </div>
+        <div>
+          Likes: {this.state.likes}{' '}
+          <span onClick={this.likeIncrement} style={{ color: 'red', cursor: 'pointer' }}>
+            &hearts;
+          </span>
+        </div>
+        <button onClick={this.likeDecrement}>Decrment</button>
+      </>
+    );
   }
 }
 
