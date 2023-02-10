47ee4c453c4e579d6c9bf1000727b576711d3873
noushadbm <noushadbm@gmail.com>
2023-02-10 22:19:48 +0400

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 import Post from './Post';
+import { ThemeContext } from './theme-context';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <Post/>
-    </div>
+    <ThemeContext.Provider value={2}>
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+        <Post />
+      </div>
+    </ThemeContext.Provider>
   );
 }
